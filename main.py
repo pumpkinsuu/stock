131e1216dfb748a8cf75f8f481b4f074561325a9
pumpkinsuu <pumpkinsuu@gmail.com>
2021-07-11 14:10:14 +0700

    update

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from flask_cors import CORS
 
 from route import create_route
 from model import load
+from utilities import ErrorAPI
 
 app = Flask(__name__)
 CORS(app)
 
 
+@app.errorhandler(ErrorAPI)
+def exception(e: ErrorAPI):
+    return jsonify(e.detail()), 500
+
+
 @app.errorhandler(Exception)
-def exception(e):
+def exception(e: Exception):
     return jsonify({'error': str(e)}), 500
 
 
